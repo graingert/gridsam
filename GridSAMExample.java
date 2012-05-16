c036c8d45de37ba859d9c6eb36f5ba626e69f686
Thomas Grainger <tagrain@gmail.com>
2012-05-16 21:52:30 +0100

    jsdl manipulation completed

--- GridSAMExample.java
+++ GridSAMExample.java
@@ -1,12 +1,32 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.icenigrid.gridsam.client.common.ClientSideJobManager;
-import org.icenigrid.gridsam.core.*;
+
+import org.icenigrid.gridsam.core.JobInstance;
+import org.icenigrid.gridsam.core.JobManagerException;
+import org.icenigrid.gridsam.core.SubmissionException;
+import org.icenigrid.gridsam.core.UnknownJobException;
+import org.icenigrid.gridsam.core.UnsupportedFeatureException;
 import org.icenigrid.gridsam.core.jsdl.JSDLSupport;
-import org.icenigrid.schema.jsdl.y2005.m11.*; 
+import org.icenigrid.schema.jsdl.y2005.m11.JobDefinitionDocument;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.SAXException;
 
 import org.apache.xmlbeans.XmlException; 
 
-import java.io.*;
-import java.util.*;
+import uk.co.graingert.gridsam.mapreduce.PosixJSDLCreator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import static org.joox.JOOX.$;
 
 public class GridSAMExample {
 
@@ -16,7 +36,7 @@ public class GridSAMExample {
 	public static void main(String[] args)
 		throws JobManagerException, SubmissionException, UnsupportedFeatureException, UnknownJobException,
 			IOException, XmlException, InterruptedException {
-
+		
 		System.out.println("Creating a new client Job Manager...");
 		ClientSideJobManager jobManager = new ClientSideJobManager(
 			new String[] { "-s", gridsamServer },
@@ -38,6 +58,32 @@ public class GridSAMExample {
 	}
 
 	private static String createJSDLDescription(String execName, String args) {
-		return "";
+		String s_jsdl = "";
+		try {
+			InputStream in = PosixJSDLCreator.class.getResourceAsStream("posix.jsdl");
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db;
+			db = dbf.newDocumentBuilder();
+			Document jsdl = db.parse(in);
+			
+			$(jsdl).find("#execName").text(execName);
+			$(jsdl).find("#args").text(args);
+			$(jsdl).find("URI.ftp").text(ftpServer);
+			
+		    DOMImplementationLS domImplementation = (DOMImplementationLS) jsdl.getImplementation();
+		    LSSerializer lsSerializer = domImplementation.createLSSerializer();
+		    s_jsdl = lsSerializer.writeToString(jsdl); 
+
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return s_jsdl;
 	}
 }
